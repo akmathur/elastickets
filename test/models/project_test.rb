80c47130ef934ae93e798cd7fb70411f3e77bf08
akmathur <akmathur@gmail.com>
2016-02-29 21:11:42 +0530

    basic CRUD unit tests for Project and Ticket models

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validations" do
+    project = Project.new
+    assert !project.valid?
+    assert_equal 2, project.errors.messages.keys.count
+    assert_equal 0, ([:name, :description] - project.errors.messages.keys).count
+
+    project = Project.new(name: "muzicmania", description: "plays maniacal music like a magician")
+    assert project.valid?
+  end
+
+  test "CRUD" do
+    projects_count = Project.count
+
+    # Create
+    project = Project.new(name: "muzicmania", description: "plays maniacal music like a magician")
+    assert project.save
+    assert_equal projects_count+1, Project.count
+
+    # Read
+    assert "muzicmania", Project.where(name: "muzicmania").first.name
+
+    # Update
+    project.name = "Music-Mania"
+    assert project.save
+    assert_equal projects_count+1, Project.count
+
+    # Delete
+    assert project.delete
+    assert projects_count, Project.count
+  end
 end
--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class TicketTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validations" do
+    ticket = Ticket.new
+    assert !ticket.valid?
+    assert_equal 2, ticket.errors.messages.keys.count
+    assert_equal 0, ([:title, :description] - ticket.errors.messages.keys).count
+
+
+    ticket = Ticket.new(project_id: projects(:turbo_sheet).id,
+                        title: "turbo sheet calculations are wrong",
+                        description: "when I divide by zero I am getting something weird")
+    assert ticket.valid?
+  end
+
+  test "CRUD" do
+    tickets_count = Ticket.count
+
+    # Create
+    ticket = Ticket.new(project_id: projects(:turbo_sheet).id,
+                        title: "turbo sheet error",
+                        description: "turbo sheet is crashing")
+    assert_difference 'Ticket.count' do
+      ticket.save
+    end
+
+    # Read
+    assert_equal ticket.project.name, projects(:turbo_sheet).name
+
+    # Update
+    assert_nothing_raised do
+      ticket.update_attributes({title: "turbo sheet crashes",
+                                 description: "crash happening while opening a large file"})
+    end
+
+    # Delete
+    assert ticket.delete
+    assert_equal tickets_count, Ticket.count
+  end
 end
